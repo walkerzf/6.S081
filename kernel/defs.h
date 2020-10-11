3b6d1dc214c4b4869f1b0d485a3e7a7e1aa3d7a8
walkerzf <zf21910036@zju.edu.cn>
2020-10-11 16:56:03 +0800

    complete print a pgtbl

--- kernel/defs.h
+++ kernel/defs.h
@@ -158,6 +158,7 @@ void            uartputc_sync(int);
 int             uartgetc(void);
 
 // vm.c
+void            vmprint(pagetable_t);
 void            kvminit(void);
 void            kvminithart(void);
 uint64          kvmpa(uint64);
@@ -178,6 +179,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+
 
 // plic.c
 void            plicinit(void);
