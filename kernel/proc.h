5e9b4af98d37ebe4f2a9e6158d415fd3940a7b5b
walkerzf <zf21910036@zju.edu.cn>
2020-10-26 20:46:58 +0800

    prepration for lab4 part 3

--- kernel/proc.h
+++ kernel/proc.h
@@ -103,4 +103,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int interval;                // the first argument interval
+  uint64  fp;                  // the second argument function pointer 
+  int pastedticks;             //track of past ticked
 };
