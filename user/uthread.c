64a9ae7edb16de7fccbb02d7a72d60383cb76887
walkerzf <zf21910036@zju.edu.cn>
2020-11-14 16:43:07 +0800

    uthread.c done

--- user/uthread.c
+++ user/uthread.c
@@ -12,6 +12,21 @@
 
 
 struct thread {
+  uint64 ra;
+  uint64 sp;
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
 
@@ -30,6 +45,7 @@ thread_init(void)
   // a RUNNABLE thread.
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
+  
 }
 
 void 
@@ -63,6 +79,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t,(uint64)current_thread);
   } else
     next_thread = 0;
 }
@@ -77,6 +94,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ra = (uint64)func;
+  t->sp = (uint64)(t->stack+STACK_SIZE);
 }
 
 void 
